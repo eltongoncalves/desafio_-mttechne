1c2c94db8b9eff0800f308525ba990835aff1c71
Elton Goncalves <elton@dobemcontabilidade.com>
2023-04-07 00:39:32 -0300

    Deploy docker

--- lancamentos/src/main/java/com/mttech/lancamentos/controller/LancamentoController.java
+++ lancamentos/src/main/java/com/mttech/lancamentos/controller/LancamentoController.java
@@ -1,6 +1,7 @@
 package com.mttech.lancamentos.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +17,12 @@ public class LancamentoController {
     private LancamentoService lancamentoService;
 
     @PostMapping("/credito") 
-    public LancamentoDto credito( @RequestBody LancamentoDto dto) throws Exception{
+    public LancamentoDto credito( @RequestBody @Validated LancamentoDto dto) throws Exception{
         return lancamentoService.lancarCredito(dto);
     }
 
     @PostMapping("/debito") 
-    public LancamentoDto debito( @RequestBody LancamentoDto dto) throws Exception{
+    public LancamentoDto debito( @RequestBody  @Validated LancamentoDto dto) throws Exception{
         return lancamentoService.lancarDebito(dto);
     }
     
